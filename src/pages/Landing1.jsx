c2589fd851099b2a54bfcd51b342dbd4338d0ddd
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-12-28 17:49:44 +0530

    fix

--- src/pages/Landing1.jsx
+++ src/pages/Landing1.jsx
@@ -498,7 +498,10 @@ const EnhancedRealEstateLanding = () => {
         </div>
       </div>
       <>
-        <div className="bg-gradient-to-r from-blue-600 to-blue-800 text-white pt-16 pb-24 relative overflow-hidden">
+        <div
+          className="bg-gradient-to-r from-blue-600 to-blue-800 text-white pt-16 pb-24 relative overflow-hidden"
+          id="try-assistant"
+        >
           <div className="absolute inset-0 opacity-10">
             <div className="absolute top-0 left-0 w-96 h-96 bg-white rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"></div>
             <div className="absolute bottom-0 right-0 w-96 h-96 bg-blue-400 rounded-full blur-3xl translate-x-1/2 translate-y-1/2"></div>
